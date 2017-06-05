1efcdcb63f9019067b938c58c8545c12852d3940
Tristan Huber <tristan.huber.206@gmail.com>
2017-06-05 15:45:18 -0700

    typescript and react

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,41 @@
 module.exports = {
-	entry: "./entry.js",
+	entry: "./src/index.tsx",
 	output: {
-		path: __dirname,
+		path: __dirname + "/dist",
 		filename: "bundle.js"
 	},
-	module: {
-		rules: [{
-			test: /\.less$/,
-			use: [{
-				loader: "style-loader"
-			}, {
-				loader: "css-loader"
-			}, {
-				loader: "less-loader"
-			}]
-		}]
 
+	devtool: "source-map",
+
+	resolve: {
+		extensions: [".ts", ".tsx", ".js", ".json"]
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.less$/,
+				use: [{
+					loader: "style-loader"
+				}, {
+					loader: "css-loader"
+				}, {
+					loader: "less-loader"
+				}]
+			},
+			{
+				test: /\.tsx?$/, loader: "awesome-typescript-loader"
+			},
+			{
+				enforce: "pre", test: /\.js$/, loader: "source-map-loader"
+			}
+
+		]
+
+	},
+
+	externals: {
+		"react": "React",
+		"react-dom": "ReactDOM"
 	}
 };
